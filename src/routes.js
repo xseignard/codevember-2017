05bc1ad5c8d54bb7f971a05f5d778b52a289a015
xseignard <xavier.seignard@gmail.com>
2017-11-06 20:17:23 +0100

    :dart: new build with hashrouter

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
 
 import Home from './components/Home';
@@ -8,12 +8,12 @@ import D1 from './day/D1';
 const history = createBrowserHistory();
 
 const Routes = props => (
-	<BrowserRouter>
+	<HashRouter>
 		<div>
 			<Route exact path="/D1" component={D1} />
 			<Route exact path="/" component={Home} />
 		</div>
-	</BrowserRouter>
+	</HashRouter>
 );
 
 export default Routes;
